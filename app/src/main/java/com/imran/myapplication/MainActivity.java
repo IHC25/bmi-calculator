ee752e8db72d63669a2137fbd55ac929fdd67be4
IHC25 <imranhchy@gmail.com>
2024-10-02 15:02:26 +0600

    basic bmi calculation & displaying result is added

--- app/src/main/java/com/imran/myapplication/MainActivity.java
+++ app/src/main/java/com/imran/myapplication/MainActivity.java
@@ -1,6 +1,10 @@
 package com.imran.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edWeight,edFeet,edInches;
+    Button calculateButton;
+    TextView resultDisplay,bmiLevel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +27,58 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        edWeight = findViewById(R.id.edWeight);
+        edFeet = findViewById(R.id.edFeet);
+        edInches = findViewById(R.id.edInches);
+        calculateButton = findViewById(R.id.calculateButton);
+        resultDisplay = findViewById(R.id.resultDisplay);
+        bmiLevel = findViewById(R.id.bmiLevel);
+
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String sWeight, sFeet, sInches;
+                Double userWeight,userFeet, userInches, userHeight, bmiResult;
+
+                sWeight = edWeight.getText().toString();
+                sFeet = edFeet.getText().toString();
+                sInches = edInches.getText().toString();
+
+                userWeight = Double.parseDouble(sWeight);
+                userFeet = Double.parseDouble(sFeet);
+                userInches = Double.parseDouble(sInches);
+                userHeight = (0.3048 * userFeet) + (0.0254 * userInches);
+
+                bmiResult = userWeight/(userHeight*userHeight);
+                String bmiResultRounded = String.format("%.1f",bmiResult);
+
+                resultDisplay.setText(bmiResultRounded);
+
+                if(bmiResult<18.5){
+                    bmiLevel.setText("Underweight");
+                    bmiLevel.setTextColor(getResources().getColor(R.color.light_green));
+                    bmiLevel.setVisibility(View.VISIBLE);
+                } else if (bmiResult>18.5 && bmiResult<=24.9) {
+                    bmiLevel.setText("Normal");
+                    bmiLevel.setTextColor(getResources().getColor(R.color.green));
+                    bmiLevel.setVisibility(View.VISIBLE);
+                } else if (bmiResult>24.9 && bmiResult<=29.9) {
+                    bmiLevel.setText("Excess Weight");
+                    bmiLevel.setTextColor(getResources().getColor(R.color.orange));
+                    bmiLevel.setVisibility(View.VISIBLE);
+                } else if (bmiResult>29.9 && bmiResult<=34.9) {
+                    bmiLevel.setText("Obesity");
+                    bmiLevel.setTextColor(getResources().getColor(R.color.light_red));
+                    bmiLevel.setVisibility(View.VISIBLE);
+                } else {
+                    bmiLevel.setText("Extremely Obese");
+                    bmiLevel.setTextColor(getResources().getColor(R.color.red));
+                    bmiLevel.setVisibility(View.VISIBLE);
+                }
+
+            }
         });
     }
 }
