e2d06e94a56a5e0fc3797fae72977a5f960a2bf1
IHC25 <imranhchy@gmail.com>
2024-10-03 11:00:01 +0600

    style changed & comments for bmi result added

--- app/src/main/java/com/imran/myapplication/MainActivity.java
+++ app/src/main/java/com/imran/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,7 +19,8 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
     EditText edWeight,edFeet,edInches;
     Button calculateButton;
-    TextView resultDisplay,bmiLevel;
+    TextView resultDisplay,bmiLevel, bmiComments;
+    ImageView footerImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
         calculateButton = findViewById(R.id.calculateButton);
         resultDisplay = findViewById(R.id.resultDisplay);
         bmiLevel = findViewById(R.id.bmiLevel);
+        bmiComments = findViewById(R.id.bmiComments);
+        footerImage = findViewById(R.id.footerImage);
 
         //Functionality of calculate button
         calculateButton.setOnClickListener(new View.OnClickListener() {
@@ -66,29 +70,48 @@ public class MainActivity extends AppCompatActivity {
                     Locale Locale = java.util.Locale.US;
                     String bmiResultRounded = String.format(Locale,"%.1f",bmiResult);
 
+
                     resultDisplay.setText(bmiResultRounded);
 
+                    footerImage.setVisibility(View.GONE);
+
+                    String underWeightAdvice, normalWeightAdvice, excessWeightAdvice, obesityAdvice;
+                    underWeightAdvice = "A BMI of less than 18.5 indicates that you are underweight, so you may need to put on some weight. You are recommended to ask your doctor or a dietitian for advice.";
+                    normalWeightAdvice = "A BMI of 18.5-24.9 indicates that you are at a healthy weight for your height. By maintaining a healthy weight, you lower your risk of developing serious health problems.";
+                    excessWeightAdvice = "A BMI of 25-29.9 indicates that you are slightly overweight. You may be advised to lose some weight for health reasons. You are recommended to talk to your doctor or a dietitian for advice.";
+                    obesityAdvice = "A BMI of over 30 indicates that you are heavily overweight. Your health may be at risk if you do not lose weight. You are recommended to talk to your doctor or a dietitian for advice.";
                     //Showing user their BMI level
                     if(bmiResult<18.5){
                         bmiLevel.setText("Underweight");
+                        bmiComments.setText(underWeightAdvice);
                         bmiLevel.setTextColor(getResources().getColor(R.color.light_green));
                         bmiLevel.setVisibility(View.VISIBLE);
+                        bmiComments.setVisibility(View.VISIBLE);
+
                     } else if (bmiResult>18.5 && bmiResult<=24.9) {
                         bmiLevel.setText("Normal");
+                        bmiComments.setText(normalWeightAdvice);
                         bmiLevel.setTextColor(getResources().getColor(R.color.green));
                         bmiLevel.setVisibility(View.VISIBLE);
+                        bmiComments.setVisibility(View.VISIBLE);
                     } else if (bmiResult>24.9 && bmiResult<=29.9) {
                         bmiLevel.setText("Excess Weight");
+                        bmiComments.setText(excessWeightAdvice);
                         bmiLevel.setTextColor(getResources().getColor(R.color.orange));
                         bmiLevel.setVisibility(View.VISIBLE);
+                        bmiComments.setVisibility(View.VISIBLE);
                     } else if (bmiResult>29.9 && bmiResult<=34.9) {
                         bmiLevel.setText("Obesity");
+                        bmiComments.setText(obesityAdvice);
                         bmiLevel.setTextColor(getResources().getColor(R.color.light_red));
                         bmiLevel.setVisibility(View.VISIBLE);
+                        bmiComments.setVisibility(View.VISIBLE);
                     } else {
                         bmiLevel.setText("Extremely Obese");
+                        bmiComments.setText(obesityAdvice);
                         bmiLevel.setTextColor(getResources().getColor(R.color.red));
                         bmiLevel.setVisibility(View.VISIBLE);
+                        bmiComments.setVisibility(View.VISIBLE);
                     }
                 }
 
